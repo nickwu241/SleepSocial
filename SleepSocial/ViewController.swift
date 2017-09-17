aa8c701ce3e84d97d089eca6c8e33d3673c673b0
nickwu241 <nickwu241@gmail.com>
2017-09-16 19:47:08 -0700

    Initialize ios project

---
+++ SleepSocial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SleepSocial
+//
+//  Created by Nicholas Wu on 9/16/17.
+//  Copyright © 2017 Sleep Social. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
